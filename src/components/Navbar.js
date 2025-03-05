7927799062c419a7506c06e435d48fdfae7d0516
Divyansh Tamrakar <divyanshtamrakar@Divyansh-TamrakarB675s-MacBook-Pro.local>
2025-03-05 12:16:55 +0530

    feat:added navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-blue-600 p-4 text-white">
+      <div className="container mx-auto flex justify-between">
+        <Link href="/add-box" className="text-lg font-bold">
+          Shipping Calculator
+        </Link>
+        <div>
+          <Link href="/add-box" className="px-4">
+            Add Box
+          </Link>
+          <Link href="/list" className="px-4">
+            View List
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
